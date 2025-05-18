101eb28712fdc94070de1a15d53571b05c808896
Ambily P R <mail2ambilyprajan@gmail.com>
2025-05-18 11:38:30 +0530

    Model Training

--- demo.py
+++ demo.py
@@ -2,12 +2,13 @@
 
 #ins = MongoDBClient()
 
-from us_visa.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from us_visa.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 #from us_visa.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
 
 from us_visa.components.data_ingestion import DataIngestion
 from us_visa.components.data_validation import DataValidation
 from us_visa.components.data_transformation import DataTransformation
+from us_visa.components.model_trainer import ModelTrainer
 
 
 di_ins=DataIngestion(DataIngestionConfig)
@@ -23,4 +24,8 @@ dt_ins=DataTransformation(data_ingestion_artifact=di_art,data_transformation_con
                            data_validation_artifact=dv_art)
 
 
-dv_art=dt_ins.initiate_data_transformation()
+dt_art=dt_ins.initiate_data_transformation()
+
+mt_ins=ModelTrainer(data_transformation_artifact=dt_art,model_trainer_config=ModelTrainerConfig)
+
+mt_art=mt_ins.initiate_model_trainer()
