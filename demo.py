c18831916485dd2c44fac58b18e5d65b1c23b20d
Ambily P R <mail2ambilyprajan@gmail.com>
2025-05-18 15:55:46 +0530

    AWS connection

--- demo.py
+++ demo.py
@@ -1,31 +1,35 @@
-#from us_visa.configuration.mongo_db_connection import MongoDBClient
+# from us_visa.configuration.mongo_db_connection import MongoDBClient
 
-#ins = MongoDBClient()
+# ins = MongoDBClient()
 
-from us_visa.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
-#from us_visa.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
+# from us_visa.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
+# #from us_visa.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
 
-from us_visa.components.data_ingestion import DataIngestion
-from us_visa.components.data_validation import DataValidation
-from us_visa.components.data_transformation import DataTransformation
-from us_visa.components.model_trainer import ModelTrainer
-
-
-di_ins=DataIngestion(DataIngestionConfig)
-
-di_art=di_ins.initiate_data_ingestion()
+# from us_visa.components.data_ingestion import DataIngestion
+# from us_visa.components.data_validation import DataValidation
+# from us_visa.components.data_transformation import DataTransformation
+# from us_visa.components.model_trainer import ModelTrainer
 
 
-dv_ins=DataValidation(data_ingestion_artifact=di_art,data_validation_config=DataValidationConfig)
+# di_ins=DataIngestion(DataIngestionConfig)
 
-dv_art=dv_ins.initiate_data_validation()
-
-dt_ins=DataTransformation(data_ingestion_artifact=di_art,data_transformation_config=DataTransformationConfig,
-                           data_validation_artifact=dv_art)
+# di_art=di_ins.initiate_data_ingestion()
 
 
-dt_art=dt_ins.initiate_data_transformation()
+# dv_ins=DataValidation(data_ingestion_artifact=di_art,data_validation_config=DataValidationConfig)
 
-mt_ins=ModelTrainer(data_transformation_artifact=dt_art,model_trainer_config=ModelTrainerConfig)
+# dv_art=dv_ins.initiate_data_validation()
 
-mt_art=mt_ins.initiate_model_trainer()
+# dt_ins=DataTransformation(data_ingestion_artifact=di_art,data_transformation_config=DataTransformationConfig,
+#                            data_validation_artifact=dv_art)
+
+
+# dt_art=dt_ins.initiate_data_transformation()
+
+# mt_ins=ModelTrainer(data_transformation_artifact=dt_art,model_trainer_config=ModelTrainerConfig)
+
+# mt_art=mt_ins.initiate_model_trainer()
+
+
+from us_visa.configuration.aws_connection import S3Client
+ins=S3Client()
